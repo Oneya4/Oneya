6039f3f8fd75e68eed2402f437b339bcbee47629
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2020-09-01 10:21:34 +0300

    First upload 2.0 fixing screen refactor

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
-// /*progress bar*/
-// window.onscroll = function() {myScroll()};
-// function myScroll(){
-// 	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-//   	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-//   	var scrolled = (winScroll / height) * 100;
-//   	document.getElementById("Progbar").style.width = scrolled + "%";
+/*progress bar*/
+window.onscroll = function() {myScroll()};
+function myScroll(){
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  	var scrolled = (winScroll / height) * 100;
+  	document.getElementById("Progbar").style.width = scrolled + "%";
+}
 
 
 function openNav() {
