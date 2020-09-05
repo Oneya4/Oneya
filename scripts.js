9182d853cdbbf3745655cc5b1f5ff8aeb306bedb
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2020-09-06 00:47:23 +0300

    contact section first upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-var http = require('http');
-http.createServer(function(req, res){
-	res.writeHead(303,{'Content-Type':'text/html'})
-});
+// var http = require('http');
+// http.createServer(function(req, res){
+// 	res.writeHead(303,{'Content-Type':'text/html'})
+// });
 
 /*progress bar*/
 window.onscroll = function() {myScroll()};
